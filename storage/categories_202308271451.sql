08a2f7fd29cc13a33f9c255e6116dc8600ec881f
Luís Henrique <79663087+luishenriqueturani@users.noreply.github.com>
2023-08-27 14:52:56 -0300

    Refeitas as exportações

---
+++ storage/categories_202308271451.sql
@@ -0,0 +1,3 @@
+INSERT INTO fipe.categories (id,label,created_at,updated_at,deleted_at) VALUES
+	 ('1561d68a-4502-11ee-94e1-e86d65b35387','caminhão','2023-08-27 14:49:34',NULL,NULL),
+	 ('1746cb03-4502-11ee-94e1-e86d65b35387','moto','2023-08-27 14:49:37',NULL,NULL);
